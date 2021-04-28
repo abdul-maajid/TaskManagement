f8fe5c72ebfc65079f30e74a897dec0414693df9
Maajid Siddiqui <msiddiqui.digitals@gmail.com>
2021-04-29 04:24:07 +0500

    First Code Push

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,101 @@
+<template>
+  <breeze-authenticated-layout>
+    <template #header>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        Dashboard
+      </h2>
+    </template>
+
+    <div class="py-12 pt-6">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+          <div class="p-6 bg-white border-b border-gray-200">
+            <div class="flex justify-between">
+              Task List
+              <breeze-button class="ml-4"> + New List </breeze-button>
+            </div>
+          </div>
+
+          <div class="relative p-2 flex overflow-x-auto h-full">
+            <!-- Columns (Statuses) -->
+            <div
+              v-for="(list, index) in lists"
+              :key="index"
+              class="mr-6 w-4/5 max-w-xs flex-1 flex-shrink-0"
+            >
+              <div class="rounded-md shadow-md overflow-hidden">
+                <div
+                  class="p-3 flex justify-between items-baseline bg-blue-800"
+                >
+                  <h4 class="font-medium text-white">
+                    {{ list.title }}
+                  </h4>
+                  <button
+                    class="py-1 px-2 text-sm text-orange-500 hover:underline"
+                  >
+                    Add Task
+                  </button>
+                </div>
+                <div
+                  class="p-2 flex-1 flex flex-col h-full overflow-x-hidden overflow-y-auto bg-blue-100"
+                >
+                  <!-- Tasks -->
+                  <div
+                    v-for="task in list.tasks"
+                    :key="task.id"
+                    class="mb-3 p-3 h-24 flex flex-col bg-white rounded-md shadow transform hover:shadow-md cursor-pointer"
+                  >
+                    <span class="block mb-2 text-xl text-gray-900">
+                      {{ task.title }}
+                    </span>
+                    <p class="text-gray-700 truncate">
+                      {{ task.description }}
+                    </p>
+                  </div>
+                  <!-- ./Tasks -->
+
+                  <!-- No Tasks -->
+                  <div
+                    v-show="!list.tasks.length"
+                    class="flex-1 p-4 flex flex-col items-center justify-center"
+                  >
+                    <span class="text-gray-600">No tasks yet</span>
+                    <button
+                      class="mt-1 text-sm text-orange-600 hover:underline"
+                    >
+                      Add one
+                    </button>
+                  </div>
+                  <!-- ./No Tasks -->
+                </div>
+              </div>
+            </div>
+            <!-- ./Columns -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </breeze-authenticated-layout>
+</template>
+
+<script>
+import BreezeAuthenticatedLayout from "@/Layouts/Authenticated";
+import BreezeButton from "@/Components/Button";
+
+export default {
+  components: {
+    BreezeButton,
+    BreezeAuthenticatedLayout,
+  },
+
+  props: {
+    auth: Object,
+    errors: Object,
+    lists: Array,
+  },
+
+  mounted() {
+    console.log(this.lists);
+  },
+};
+</script>
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\TaskList;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
+
+Route::get('/', function () {
+    $lists = TaskList::with('tasks')->get();
+    return Inertia::render('Dashboard', ['lists' => $lists]);
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__ . '/auth.php';
